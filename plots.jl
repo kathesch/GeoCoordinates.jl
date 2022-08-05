811ccccb1477bb031288ead6bc6bbd85ad3028e7
kathesch <katherinegruenewald@gmail.com>
2022-08-04 20:43:24 -0600

    init

---
+++ plots.jl
@@ -0,0 +1,55 @@
+using GLMakie, DataFrames, CSV, FileIO
+using Downloads: download
+
+#Data Import
+#=
+df = CSV.File("SciTec_code_problem_data.csv", header=["T","ϕ","λ","h"]) |> DataFrame
+
+transform!(df, :h => ByRow(x->1000*x) => :h)
+transform!(df, [:ϕ, :λ, :h] => ByRow(lla2xyz) => :P)
+transform!(df, :P => [:X,:Y,:Z])
+transform!(df, :T => (x->x - lag(x, default=x[1] - 1)) => :dT)
+transform!(df, :P => (x->x - lag(x,default=x[1])) => :dP)
+transform!(df, [:dP,:dT] => ByRow(./) => :V)
+transform!(df, :V => [:Vx, :Vy, :Vz])
+
+t1 = 1532334000
+t2 = 1532335268
+
+v1 = lin_inter(t1, df.T,df.V)
+v2 = lin_inter(t2, df.T,df.V)
+
+p1 = lin_inter(t1, df.T,df.P)
+p2 = lin_inter(t2, df.T,df.P)
+
+using GLMakie, FileIO
+using Downloads: download
+
+earth_img = load(download("https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Blue_Marble_Next_Generation_+_topography_+_bathymetry.jpg/1024px-Blue_Marble_Next_Generation_+_topography_+_bathymetry.jpg"))
+
+n = 512
+lats = LinRange(90,-90, n) 
+longs = LinRange(-180,180, 2 * n)
+pos = [lla2xyz(ϕ,λ,0.) for ϕ in lats, λ in longs]
+x,y,z = [[i[j] for i in pos] for j in 1:3]
+
+
+begin
+    f = Figure()
+    ax = Axis3(f[1,1],viewmode=:fitzoom,aspect=:data,)
+    hidedecorations!(ax)
+    hidespines!(ax)
+    
+    surface!(ax, x,y,z,color=earth_img)
+    lines!(ax,df.P .|> Point3, color=:red,linewidth=2)
+    
+    n = 20
+    quiver!(ax,df.P[1:n:end] .|> x->Point3(x), df.V[1:n:end] .|> x->Point3(x) .* 1000, color=:violet,linewidth=10000*2,arrowsize=100000)
+
+    scatter!(ax,[p1,p2] .|> Point3, color=:orange, markersize=10)
+    quiver!(ax,[p1,p2] .|> Point3, [v1,v2] .|> x->Point3(x) .* 1000, color=:orange,linewidth=20000*2,arrowsize=100000)
+    f
+end
+
+save("earth_viz.png",f)
+=#
