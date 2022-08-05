dbd3cf8d68b604fbd9ef9ea87c02fd52b1be8db0
kathesch <katherinegruenewald@gmail.com>
2022-08-05 10:58:45 -0600

    csv file added

--- src/GeoCoordinates.jl
+++ src/GeoCoordinates.jl
@@ -150,7 +150,7 @@ end
 Returns a tuple of the dataframe and the velocity at the interpolated unix time `t` in the scitec data set. 
 """
 function scitec_data(t)
-    df = CSV.File("SciTec_code_problem_data.csv", header=["T","ϕ","λ","h"]) |> DataFrame
+    df = CSV.File("../data/SciTec_code_problem_data.csv", header=["T","ϕ","λ","h"]) |> DataFrame
 
     #converts altitude to meters
     transform!(df, :h => ByRow(x->1000*x) => :h)
